59ce261159df18d36fc5f6ee6af12bd776e19a0f
emeraldjava <emeraldjava@protonmail.com>
2024-05-04 13:34:06 +0100

    Use docs dir

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # emeraldjava-astro
 
-emeraldjava-astro
+https://github.com/emeraldjava/emeraldjava-astro
+
+## 04-05-2024
 
 - https://github.com/withastro/github-pages
 - https://about.gitlab.com/blog/2022/10/24/publishing-an-astro-site-with-pages/
--- astro.config.ts
+++ astro.config.ts
@@ -40,10 +40,10 @@ export default defineConfig({
   // https://about.gitlab.com/blog/2022/10/24/publishing-an-astro-site-with-pages/
   // GitLab Pages requires exposed files to be located in a folder called "public".
   // So we're instructing Astro to put the static build output in a folder of that name.
-  //outDir: 'public',
+  outDir: 'docs',
 
   // The folder name Astro uses for static files (`public`) is already reserved
   // for the build output. So in deviation from the defaults we're using a folder
   // called `static` instead.
-  //publicDir: 'static',
+  publicDir: 'static'
 });
