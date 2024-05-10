681c676397f398b89ef9e980435ee640e2f2ef63
emeraldjava <emeraldjava@protonmail.com>
2024-05-10 07:30:52 +0100

    Use dist folder

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import { build } from "astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -40,8 +41,9 @@ export default defineConfig({
   // https://about.gitlab.com/blog/2022/10/24/publishing-an-astro-site-with-pages/
   // GitLab Pages requires exposed files to be located in a folder called "public".
   // So we're instructing Astro to put the static build output in a folder of that name.
-  outDir: 'docs',
+  outDir: './dist',
 
+  output: 'static',
   // The folder name Astro uses for static files (`public`) is already reserved
   // for the build output. So in deviation from the defaults we're using a folder
   // called `static` instead.
